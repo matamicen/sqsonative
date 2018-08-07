f1f4b29a2e9e6d0dc8f170e6f4b01b9bbe55a46c
Matias Micenmacher <matiasmicenmacher@MacBook-Pro-de-Matias.local>
2018-08-06 23:19:20 -0300

    anda QslScanQR android e iOS

--- src/components/QslScan/QslScanScreen.js
+++ src/components/QslScan/QslScanScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, Image, View, Button, ActivityIndicator, TouchableOpacity, ScrollView, Modal } from 'react-native';
+import { Text, Image, View, StyleSheet, Button, ActivityIndicator, TouchableOpacity, ScrollView, Modal } from 'react-native';
 import { connect } from 'react-redux';
 import { NavigationActions } from 'react-navigation';
 import QsoHeader from './QsoHeader';
@@ -7,6 +7,7 @@ import { getQslScan } from '../../actions';
 import MediaImages from './MediaImages';
 import Likes from './Likes';
 import Comments from './Comments';
+import QRCodeScanner from 'react-native-qrcode-scanner';
 
 class QslScanScreen extends Component {
   static navigationOptions = {
@@ -19,7 +20,8 @@ class QslScanScreen extends Component {
     
     this.state = {
       conta: 0,
-      actindicatorfecthQslCard: false
+      actindicatorfecthQslCard: false,
+      scanQR: false
       
     };
   }
@@ -49,14 +51,59 @@ ScanQSL = async () => {
   
   }
      else this.props.getQslScan("555d1aa6-81f8-11e8-ae0b-061cacc9b2a2");
+                                 
 
       this.setState({conta: 1})
 
    }
+
+   scanQR = async function() {
+    this.setState({scanQR: !this.state.scanQR})
+  //  Linking
+  //     .openURL('https://www.cnn.com')
+  //     .catch(err => console.error('An error occured', err));
+
+  }
+
+ 
+
+  onSuccess = async function(e) {
+ // onSuccess(e) {
+ // 
+    // Linking
+    //   .openURL(e.data)
+    //   .catch(err => console.error('An error occured', err));
+    //this.ScanQSL2(e);
+     this.setState({actindicatorfecthQslCard: true})
+    console.log('el codigo Scaneado es: ' +e.data);
+    await this.props.getQslScan(e.data);
+    this.setState({actindicatorfecthQslCard: false})
+     this.setState({scanQR: !this.state.scanQR})
+     
+
+    //   this.setState({scanQR: !this.state.scanQR})
+  }
    
 render() { console.log("RENDER QSL SCAN SCREEN!" );
 
 return   <View style={{flex: 1}}>
+      {this.state.scanQR &&
+      <QRCodeScanner
+               onRead={this.onSuccess.bind(this)}
+             // onRead={() => this.ScanQSL2(this)}
+              topContent={
+                <Text style={styles.centerText}>
+                  Go to <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on your computer and scan the QR code.
+                </Text>
+              }
+              bottomContent={
+                <TouchableOpacity style={styles.buttonTouchable}>
+                  <Text style={styles.buttonText}>OK. Got it!</Text>
+                </TouchableOpacity>
+              }
+            />
+            }
+            
        
       
        <View style={{flex: 0.26}}>
@@ -128,7 +175,7 @@ return   <View style={{flex: 1}}>
        
       
 
-       <TouchableOpacity  style={{marginLeft:10}}  onPress={ () => this.ScanQSL() }>
+       <TouchableOpacity  style={{marginLeft:10}}  onPress={ () => this.scanQR() }>
           
             <Image source={require('../../images/qrcodescan.png')}  style={{width: 33, height: 33, marginLeft: 9 } } 
          resizeMode="contain" />    
@@ -150,6 +197,44 @@ return   <View style={{flex: 1}}>
 
 
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'black'
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20
+  },
+  centerText: {
+    flex: 1,
+    fontSize: 18,
+    padding: 32,
+    color: '#777',
+  },
+  textBold: {
+    fontWeight: '500',
+    color: '#000',
+  },
+  buttonText: {
+    fontSize: 21,
+    color: 'rgb(0,122,255)',
+  },
+  buttonTouchable: {
+    padding: 16,
+  },
+});
 
  const mapStateToProps = state => {
     return {  
